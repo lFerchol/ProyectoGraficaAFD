75ba631266e126aeed5e4a02784f0a4b13408d84
lFerchol <cadesluisxTo22@gmail.com>
2018-11-05 22:54:23 -0600

    good
    Yeah

--- src/proyectograficaafd/ProyectoGraficaAFD.java
+++ src/proyectograficaafd/ProyectoGraficaAFD.java
@@ -16,7 +16,7 @@ public class ProyectoGraficaAFD {
      */
     public static void main(String[] args) {
         System.out.println("Hola");
-        /System.out.println("ok");
+        //System.out.println("ok");
     }
     
 }
